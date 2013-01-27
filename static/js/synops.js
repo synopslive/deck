1ceca60456b08aefb88a274f506e3bd9164bb85a
Stanislas Signoud <signez@stanisoft.net>
2013-01-27 01:44:51 +0100

    Fixing erratic behaviour on cartons.

--- static/js/synops.js
+++ static/js/synops.js
@@ -10,10 +10,42 @@
 var timeout_change1 = null;
 var timeout_change2 = null;
 
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+        "use strict";
+        if (this == null) {
+            throw new TypeError();
+        }
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (len === 0) {
+            return -1;
+        }
+        var n = 0;
+        if (arguments.length > 1) {
+            n = Number(arguments[1]);
+            if (n != n) { // shortcut for verifying if it's NaN
+                n = 0;
+            } else if (n != 0 && n != Infinity && n != -Infinity) {
+                n = (n > 0 || -1) * Math.floor(Math.abs(n));
+            }
+        }
+        if (n >= len) {
+            return -1;
+        }
+        var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+        for (; k < len; k++) {
+            if (k in t && t[k] === searchElement) {
+                return k;
+            }
+        }
+        return -1;
+    }
+}
+
 function changeTo(id){
     var next = $(".carton[data-cartonid=\"" + id +  "\"]");
     var current = $(".carton.selected");
-    var tohide = "h1, .tagline, .infos, .social-event, .social-talk";
 
     if(timeout_change1) window.clearTimeout(timeout_change1);
     if(timeout_change2) window.clearTimeout(timeout_change2);
@@ -25,8 +57,8 @@ function changeTo(id){
     if($carton.length > 1){
         next.addClass("next").removeClass('up');
 
-        $(".refcarton").removeClass("selected");
-        $(".refcarton[data-cartonid=" + id +  "]").addClass("selected");
+        $(".refcarton").removeClass("selected").closest('.jour').removeClass('selected');
+        $(".refcarton[data-cartonid=" + id +  "]").addClass("selected").closest('.jour').addClass('selected');
 
         current.removeClass('up');
         timeout_change1 = window.setTimeout(function(){
@@ -43,14 +75,27 @@ function changeTo(id){
 
 var current_id = 0;
 var change_interval = null;
+var carton_ids = [];
 
 function nextCarton(){
-    var nextone = $(".refcarton[data-cartonid=" + current_id + "]").next();
+    var currentIndex = carton_ids.indexOf(parseInt(current_id, 10));
+    var newIndex = 0;
+
+    console.log("Current idx > " + currentIndex);
+
+    if(currentIndex + 1 < carton_ids.length) {
+        newIndex = currentIndex + 1;
+    }
+
+    console.log("  New index > " + newIndex + " = " + carton_ids[newIndex]);
+
+    var nextone = $(".refcarton[data-cartonid=" + carton_ids[newIndex] + "]");
     if(!nextone.length){
         nextone = $(".refcarton:first");
     }
 
     changeTo(nextone.attr("data-cartonid"));
+    console.log("Current_id > " + current_id)
 }
 
 function resetChangeInterval(){
@@ -66,16 +111,29 @@ function resetChangeInterval(){
 $(document).ready(function(){
     var len = $(".carton").length;
 
-    if($("#page.home .carton.highlighted").length){
-        changeTo($("#page.home .carton.highlighted:first").attr("data-cartonid"));
+    var $refcarton = $(".refcarton");
+
+    $refcarton.each(function() {
+        var cid = parseInt($(this).data("cartonid"), 10);
+
+        if(carton_ids.indexOf(cid) === -1) {
+            carton_ids.push(cid);
+        }
+        console.log(carton_ids);
+        console.log(carton_ids.length);
+        console.log(carton_ids.indexOf(cid));
+    });
+
+    if($(".carton.highlighted").length){
+        changeTo($(".carton.highlighted:first").data("cartonid"));
     }else{
         changeTo($(".refcarton:first").attr("data-cartonid"));
     }
 
     resetChangeInterval();
 
-    $(".refcarton").click(function(){
-       changeTo($(this).attr("data-cartonid"));
+    $refcarton.click(function(){
+       changeTo($(this).data("cartonid"));
 
        resetChangeInterval();
     });

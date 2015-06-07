02cf900cb363e5c94efdab0c89d115fdf02f20dd
Stanislas Signoud <stan@stanisoft.net>
2015-06-07 18:13:36 +0200

    Add countdown header

--- templatetags/header.py
+++ templatetags/header.py
@@ -1,5 +1,6 @@
 from django import template
 from deck.models import Show
+from datetime import datetime, date
 
 register = template.Library()
 
@@ -7,6 +8,10 @@ register = template.Library()
 @register.inclusion_tag('components/header.html')
 def header():
     all_shows = Show.objects.filter(archived=False).order_by("category__name", "short")
+
+    days_left = (datetime(2015, 6, 23).date() - date.today()).days
+
     return {
-        'all_shows': all_shows
+        'all_shows': all_shows,
+        'days_left': days_left
     }
